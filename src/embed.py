702f2933802b84b78d1619de373e279c29b1a8df
EgorShibaev <mister.egor03@gmail.com>
2023-11-14 21:48:57 +0100

    added one more model

--- src/embed.py
+++ src/embed.py
@@ -1,21 +1,24 @@
 from sentence_transformers import SentenceTransformer
+import cohere
+import numpy as np
 
 model_names = [
     'SentenceTransformer/bert-base-nli-mean-tokens',
+    'cohere'
     # TODO: add more models
 ]
 
+cohere_key = 'oFd6NCO7dkVeFVPSXsANEzlT4OShQKGovZNhcszU' # this is trial key
 
-def get_model(model_name):
+def embed(model_name, sentences):
     if model_name not in model_names:
         raise ValueError(f'Invalid model name: {model_name}')
     
-    if model_name == 'SentenceTransformer/bert-base-nli-mean-tokens':
-        return SentenceTransformer('bert-base-nli-mean-tokens')
-    else:
-        pass
-
-
-def embed(model_name, sentences):
-    model = get_model(model_name)
-    return model.encode(sentences)
+    match model_name:
+        case 'SentenceTransformer/bert-base-nli-mean-tokens':
+            model = SentenceTransformer('bert-base-nli-mean-tokens')
+            return model.encode(sentences)
+        case 'cohere':
+            co = cohere.Client(cohere_key)
+            embs = co.embed(sentences, input_type='clustering', model='embed-english-v3.0').embeddings
+            return np.array(embs)
