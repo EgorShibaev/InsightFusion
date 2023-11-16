8d7a21fb080faaec21757435f50046a66a32e933
Egor Shibaev <mister.egor03@gmail.com>
2023-11-16 11:14:34 +0100

    more modelas

--- src/embed.py
+++ src/embed.py
@@ -1,14 +1,22 @@
 from sentence_transformers import SentenceTransformer
 import cohere
 import numpy as np
+import voyageai
+from voyageai import get_embeddings
+from tqdm.notebook import tqdm
 
 model_names = [
     'SentenceTransformer/bert-base-nli-mean-tokens',
-    'cohere'
+    'cohere-english',
+    'cohere-multilingual',
+    'voyageai',  # very slow
     # TODO: add more models
 ]
 
-cohere_key = 'oFd6NCO7dkVeFVPSXsANEzlT4OShQKGovZNhcszU' # this is trial key
+# this are trial key
+cohere_key = 'oFd6NCO7dkVeFVPSXsANEzlT4OShQKGovZNhcszU'
+voyageai.api_key = 'pa-pZZbquhxGTUOMcrhyjzGN3_JmGf5uUgdt6tpImWhomY'
+
 
 def embed(model_name, sentences):
     if model_name not in model_names:
@@ -18,7 +26,17 @@ def embed(model_name, sentences):
         case 'SentenceTransformer/bert-base-nli-mean-tokens':
             model = SentenceTransformer('bert-base-nli-mean-tokens')
             return model.encode(sentences)
-        case 'cohere':
+        case 'cohere-english':
             co = cohere.Client(cohere_key)
             embs = co.embed(sentences, input_type='clustering', model='embed-english-v3.0').embeddings
+            return np.array(embs)
+        case 'cohere-multilingual':
+            co = cohere.Client(cohere_key)
+            embs = co.embed(sentences, input_type='clustering', model='embed-multilingual-v3.0').embeddings
+            return np.array(embs)
+        case 'voyageai':
+            max_batch_size = 8 # constraint from voyageai
+            embs = []
+            for i in tqdm(range(0, len(sentences), max_batch_size)):
+                embs += get_embeddings(sentences[i:i+max_batch_size])
             return np.array(embs)
