d395df2d6539f406ad07fe3412316e0d1660d6a4
EgorShibaev <mister.egor03@gmail.com>
2023-12-09 13:06:33 +0100

    decrease number of comments given to LLM

--- src/describe_by_llm.py
+++ src/describe_by_llm.py
@@ -7,22 +7,22 @@ from openai import OpenAI
 
 OPENAI_KEY = "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"
 
-def describe_claster(embeddings, comments):
+def describe_claster(embeddings, comments, top_k = 10):
     """
-    Takes top 100 comments by distance to mean and describes them.
+    Takes top top_k comments by distance to mean and describes them.
     Description is done via openai's API.
     """
 
     mean = np.mean(embeddings, axis=0)
 
-    top_100 = np.argsort(np.linalg.norm(embeddings - mean, axis=1))[:100]
+    top_k_inds = np.argsort(np.linalg.norm(embeddings - mean, axis=1))[:top_k]
 
-    top_100_comments = [comments[i] for i in top_100]
+    top_k_comments = [comments[i] for i in top_k_inds]
 
     openai = OpenAI(api_key=OPENAI_KEY)
 
     system_prompt = "You will get a list of comments, find what is common - try to describe them in one sentence."
-    user_prompt = "\n====================================\n".join(top_100_comments)
+    user_prompt = "\n====================================\n".join(top_k_comments)
 
     completion = openai.chat.completions.create(
         model="gpt-4-1106-preview",
