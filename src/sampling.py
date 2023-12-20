af9fbbe1beab6656d5fdaca4f2db0a61a05c3f1c
EgorShibaev <mister.egor03@gmail.com>
2023-12-20 08:52:36 +0100

    fixes

--- src/sampling.py
+++ src/sampling.py
@@ -4,26 +4,30 @@ from tqdm.notebook import tqdm
 from sklearn.decomposition import PCA
 
 
-def sample_from_claster(embeddings, comments, n_samples=1):
+def sample_from_claster(embeddings, comments, n_samples=1, k=10):
     """
     Samples comments from a claster.
     :param embeddings: np.array of shape (n_samples, n_features)
     :param comments: list of comments
     :param n_samples: number of samples to take
+    :param k: number of top samples to consider
     :return: list of comments
     """
+
+    if n_samples > k:
+        k = n_samples
 
     # find mean
 
     mean = np.mean(embeddings, axis=0)
 
-    # top 100 by distance to mean
+    # top k by distance to mean
 
-    top_100 = np.argsort(np.linalg.norm(embeddings - mean, axis=1))[:100]
+    top_k = np.argsort(np.linalg.norm(embeddings - mean, axis=1))[:k]
 
-    # take random n_samples from top 100
+    # take random n_samples from top k
 
-    samples = np.random.choice(top_100, size=n_samples)
+    samples = np.random.choice(top_k, size=n_samples)
 
     # return comments
 
